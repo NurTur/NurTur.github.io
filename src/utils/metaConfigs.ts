7d7b4d13162f6fcf9deb96c3a0e7e2928421088d
Nurbolat Turgynbekov <n.turgynbekov@tascredit.kz>
2023-10-14 14:37:50 +0600

    Updates

--- src/utils/metaConfigs.ts
+++
@@ -1,7 +0,0 @@
-export const basePageMeta = {
-  title: 'Developer Tutorials, Quizzes, Interviews, and Videos Itometer',
-  description:
-    'Master web and mobile development with tutorials, quizzes, exercises, videos, and interview preparation guides. Learn frontend, backend, databases, and more',
-  locale: 'en_US',
-  canonical: 'https://itometer.com',
-};
